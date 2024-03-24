91081449424d4df6870de7e1e96f61e0b3c45d97
Adam Folke <Adam.Folke@hudiksvall.se>
2024-03-24 11:35:12 +0100

    .

--- Lektion25mars/MainWindow.xaml.cs
+++ Lektion25mars/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,11 +17,71 @@ namespace Lektion25mars
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private string _filtext;
+        public string FilText
+        {
+            get { return _filtext; }
+            set
+            {
+                if (_filtext != value)
+                {
+                    _filtext = value;
+                    OnPropertyChanged(nameof(FilText));
+                }
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            FilText = "hej";
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyname)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+                openFileDialog.DefaultExt = ".txt";
+
+
+                string filePath = openFileDialog.FileName;
+                string fileContent = File.ReadAllText(filePath);
+                FilText = fileContent;
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFile(false);
+        }
+        private void SaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFile(false);
+        }
+        private void SaveFile(bool saveAs)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveAs || string.IsNullOrEmpty(saveFileDialog.FileName))
+            {
+                if (saveFileDialog.ShowDialog() != true)
+                {
+                    return;
+                }
+            }
+
+            string filePath = saveFileDialog.FileName;
+            string fileContent = FilText;
+            File.WriteAllText(filePath, fileContent);
         }
     }
 }
