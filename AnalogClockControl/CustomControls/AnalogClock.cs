3f66a84630a3842d03657653fb6f741e1589dd7a
Adam Folke <Adam.Folke@hudiksvall.se>
2024-03-24 10:57:15 +0100

    Add project files.

---
+++ AnalogClockControl/CustomControls/AnalogClock.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace AnalogClockControl.CustomControls
+{
+    public class AnalogClock : Control
+    {
+        private Line hourhand;
+        private Line minutehand;
+        private Line secondhand;
+        static AnalogClock()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AnalogClock), new FrameworkPropertyMetadata(typeof(AnalogClock)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+
+            hourhand = Template.FindName("PART_hourhand", this) as Line;
+            minutehand = Template.FindName("PART_minutehand", this) as Line;
+            secondhand = Template.FindName("PART_secondhand", this) as Line;
+            UpdateHandAngles();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += (s, e) => UpdateHandAngles();
+            timer.Start();
+            base.OnApplyTemplate();
+        }
+
+        private void UpdateHandAngles()
+        {
+            hourhand.RenderTransform = new RotateTransform((DateTime.Now.Hour / 12.0) *360, 0.5, 0.5);
+            minutehand.RenderTransform = new RotateTransform((DateTime.Now.Minute / 60.0) * 360, 0.5, 0.5);
+            secondhand.RenderTransform = new RotateTransform((DateTime.Now.Second / 60.0) * 360, 0.5, 0.5);
+        }
+    }
+}
